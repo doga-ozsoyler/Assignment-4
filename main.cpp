7bbd52aa1c9dece63d8437b5a754e58e293e1398
dogao <doga.ozsoyler@gmail.com>
2020-11-26 18:36:58 +0300

    update main to test code

--- main.cpp
+++ main.cpp
@@ -3,3 +3,19 @@
 #include "brent.cpp"
 
 using namespace std;
+
+int main(){
+
+    Brent list(11);
+
+    for(int i = 0; i < list.data_vec.size(); i++)
+    {
+        cout << i << "d=" << list.data_vec[i].data <<
+        "v=" << list.data_vec[i].valid << " ";
+    }
+
+    cout << endl;
+
+
+    return 0;
+}
