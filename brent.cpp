1f4ce250b760bb803a0aff02184a21d837fbc562
dogao <doga.ozsoyler@gmail.com>
2020-11-26 22:59:30 +0300

    add insert function

--- brent.cpp
+++ brent.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <fstream>
 #include "brent.h"
 
 using namespace std;
@@ -35,11 +37,10 @@ Brent::Brent(int table_size){
 int Brent::find_moving_cost(int h1, int h2) const{
 
     int cost = 1;
-    int i = h1;
     int noLoop = 0;
-    while(data_vec[i].valid != false && noLoop <= data_vec.size())
+    while(data_vec[h1].valid != false && noLoop <= data_vec.size())
     {
-        i = i + h2;
+        h1 = (h1 + h2) % data_vec.size();
         cost++;
         noLoop++;
     }
@@ -56,7 +57,67 @@ int Brent::find_moving_cost(int h1, int h2) const{
 
 void Brent::insert(int new_data){
 
+    if(data_vec[h1(new_data)].valid == false)
+    {
+        data_vec[h1(new_data)].data = new_data;
+        data_vec[h1(new_data)].valid = true;
+    }
+    else
+    {
+        int new_dataCost = find_moving_cost( h1(new_data), h2(new_data) );
 
+        if(new_dataCost != 0)
+        {
+            int newCost = 0;
+            int oldCost = data_vec.size() + 5;
+            int switchNum = -1;
+
+            for(int i = 0, address = h1(new_data); i < new_dataCost; i++, address = address + h2(new_data))
+            {
+                newCost = find_moving_cost( h1(data_vec[address].data), h2(data_vec[address].data) ) + i;
+
+                if(newCost < new_dataCost && newCost < oldCost)
+                {
+                    switchNum = data_vec[address].data;
+                    oldCost = newCost;
+                }
+            }
+
+            if(switchNum == -1)
+            {
+                int address = h1(new_data);
+                int inc = h2(new_data);
+
+                while(data_vec[address].valid != false)
+                {
+                    address = (address + inc) % data_vec.size();
+                }
+
+                data_vec[address].data = new_data;
+                data_vec[address].valid = true;
+            }
+            else
+            {
+                data_vec[h1(switchNum)].data = new_data;
+
+                int address = h1(switchNum);
+                int inc = h2(switchNum);
+
+                while(data_vec[address].valid != false)
+                {
+                    address = (address + inc) % data_vec.size();
+                }
+
+                data_vec[address].data = switchNum;
+                data_vec[address].valid = true;
+            }
+        }
+        else
+        {
+            cout << "List is full!!!" << endl;
+        }
+    }
+    
 }
 
 
